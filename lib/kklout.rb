4b5b395da431d956f995dc573e521f0340230810
Jesus Lopes <jtadeulopes@gmail.com>
2011-07-27 11:37:30 -0300

    Initial implementation for Klout score

---
+++ lib/kklout.rb
@@ -0,0 +1,45 @@
+require "httparty"
+
+module Klout
+
+  class Request
+    include HTTParty
+  end
+
+  class Score
+    attr_accessor :users
+    def initialize(response)
+      self.users = []
+      response["users"].each do |user|
+        self.users << User.new(user)
+      end
+    end
+  end
+
+  class User
+    attr_accessor :kscore, :twitter_screen_name
+    def initialize(user)
+      user.each {|name, value| send("#{name}=", value)}
+    end
+  end
+
+  class Client
+    attr_accessor :key
+
+    def initialize(key)
+      self.key = key
+    end
+
+    def score(usernames)
+      options = {:users => usernames, :key => self.key}
+      response = Request.get("http://api.klout.com/1/klout.json", :query => options)
+      if response.code == 200
+        Score.new(response)
+      else
+        raise "#{response.code} #{response.message}"
+      end
+    end
+
+  end
+
+end
--- spec/kklout_spec.rb
+++ spec/kklout_spec.rb
@@ -1,7 +1,42 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Kklout" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Klout do
+
+  before do
+    @klout = Klout::Client.new("wtg33wk54s6hjppr7y9sderj")
   end
+
+  context "provides score about a username" do
+
+    use_vcr_cassette "username_zigotto"
+
+    let(:score) do
+      @klout.score("zigotto")
+    end
+
+    it { score.users.should have(1).users }
+    it { score.users.first.kscore.should == 25.9 }
+    it { score.users.first.twitter_screen_name.should == "zigotto" }
+
+  end
+
+  context "provides score to many usernames" do
+
+    use_vcr_cassette "usernames_zigotto_and_jtadeulopes"
+
+    let(:score) do
+      @klout.score("zigotto,jtadeulopes")
+    end
+
+    it { score.users.should have(2).users }
+    it { score.users.first.twitter_screen_name.should == "jtadeulopes" }
+    it { score.users.first.kscore.should == 32.91 }
+    
+  end
+
+  context "when invalid username" do
+    use_vcr_cassette "invalid_username"
+    it { expect { @klout.score("alsdkfjlaskdfjlak") }.to raise_error(/404 Not Found/) }
+  end
+
 end
