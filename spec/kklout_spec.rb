1e9cc1b646eb446111812684ac1e17bc5ae81243
Jesus Lopes <jtadeulopes@gmail.com>
2011-07-27 11:55:44 -0300

    Some refactoring.

--- spec/kklout_spec.rb
+++ spec/kklout_spec.rb
@@ -6,37 +6,41 @@ describe Klout do
     @klout = Klout::Client.new("wtg33wk54s6hjppr7y9sderj")
   end
 
-  context "provides score about a username" do
+  describe Klout::Score do
 
-    use_vcr_cassette "username_zigotto"
-
-    let(:score) do
-      @klout.score("zigotto")
+    context "when invalid username" do
+      use_vcr_cassette "invalid_username"
+      it { expect { @klout.score("alsdkfjlaskdfjlak") }.to raise_error(/404 Not Found/) }
     end
 
-    it { score.users.should have(1).users }
-    it { score.users.first.kscore.should == 25.9 }
-    it { score.users.first.twitter_screen_name.should == "zigotto" }
+    context "provides score about a username" do
 
-  end
+      use_vcr_cassette "username_zigotto"
 
-  context "provides score to many usernames" do
+      let(:score) do
+        @klout.score("zigotto")
+      end
 
-    use_vcr_cassette "usernames_zigotto_and_jtadeulopes"
+      it { score.users.should have(1).users }
+      it { score.users.first.kscore.should == 25.9 }
+      it { score.users.first.twitter_screen_name.should == "zigotto" }
 
-    let(:score) do
-      @klout.score("zigotto,jtadeulopes")
     end
 
-    it { score.users.should have(2).users }
-    it { score.users.first.twitter_screen_name.should == "jtadeulopes" }
-    it { score.users.first.kscore.should == 32.91 }
-    
-  end
+    context "provides score to many usernames" do
 
-  context "when invalid username" do
-    use_vcr_cassette "invalid_username"
-    it { expect { @klout.score("alsdkfjlaskdfjlak") }.to raise_error(/404 Not Found/) }
+      use_vcr_cassette "usernames_zigotto_and_jtadeulopes"
+
+      let(:score) do
+        @klout.score("zigotto,jtadeulopes")
+      end
+
+      it { score.users.should have(2).users }
+      it { score.users.first.twitter_screen_name.should == "jtadeulopes" }
+      it { score.users.first.kscore.should == 32.91 }
+
+    end
+
   end
 
 end
